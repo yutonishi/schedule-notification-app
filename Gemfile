f7430c1b76211e06f2f4541b9e68c733b0809418
yutonishi <yuto.nishi10@gmail.com>
2022-02-01 17:25:31 +0900

    Ajax不具合修正 | 投稿、編集、削除の非同期化完成

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'pry-rails'
+gem 'simple_calendar', '~> 2.0'
--- app/javascript/packs/tasks/edit_task_modal_btn.js
+++ app/javascript/packs/tasks/edit_task_modal_btn.js
@@ -1,11 +1,11 @@
 $(document).on("click", ".edit_task_modal_btn", function(event) {
   // 表示した削除ボタン以外の部分をクリックしたとき
   if (!$(event.target).closest('.delete_task_modal_btn').length) {
-    var id = $(this).data("task-id");
-    var title = $(this).data("task-title");
-    var info = $(this).data("task-info");
-    var start_time = $(this).data("task-start_time");
-    var notification = $(this).data("task-notification");
+    var id = $(this).attr("data-task-id");
+    var title = $(this).attr("data-task-title");
+    var info = $(this).attr("data-task-info");
+    var start_time = $(this).attr("data-task-start_time");
+    var notification = $(this).attr("data-task-notification");
 
     $("#edit_task_title").val(title);
     $("#edit_task_info").val(info);
@@ -14,5 +14,11 @@ $(document).on("click", ".edit_task_modal_btn", function(event) {
     $("#edit_task_modal").attr("data-edit-task-id", id);
 
     $("#edit_task_modal").show();
+
+    console.log(id);
+    console.log(title);
+    console.log(info);
+    console.log(start_time);
+    console.log(notification);
   }
 });
