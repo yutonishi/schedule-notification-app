d11de6a266eccf5ef12110c287ce58eb7d947523
yutonishi <yuto.nishi10@gmail.com>
2022-06-20 23:22:45 +0900

    before_action追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_beginning_of_week
+  before_action :authenticate_user!
 
   def index
     @tasks = Task.belong_user(current_user.id)
